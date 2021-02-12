16e64c875b91c3e7b56a1773181247a5934b1de0
Antonio Sumak <sumakantonio@gmail.com>
2021-02-12 17:17:10 +0100

    init

--- app.js
+++ app.js
@@ -4,18 +4,26 @@ let uneseno = document.getElementById("unesensadrzaj");
 let ukloni = document.getElementById("ukloni");
 let spanbrojac = document.getElementById("broj");
 var brojac;
+let error = document.getElementById("errorp");
 
 gumb.addEventListener("click", function () {
-  var paragraf = document.createElement("p");
-  paragraf.classList.add("p-style");
-  paragraf.innerText = input.value;
-  brojac = spanbrojac.innerHTML;
-  brojac++;
-  spanbrojac.innerHTML = brojac;
-  uneseno.appendChild(paragraf);
-  input.value = "";
-  ukloni.addEventListener("click", function () {
-    uneseno.removeChild(paragraf);
-    spanbrojac.innerHTML = 0;
-  });
+  if (input.value == "") {
+    input.style.borderColor = "red";
+    error.style.display = "block";
+  } else {
+    error.style.display = "none";
+    input.style.borderColor = "#444ab8";
+    var paragraf = document.createElement("p");
+    paragraf.classList.add("p-style");
+    paragraf.innerText = input.value;
+    brojac = spanbrojac.innerHTML;
+    brojac++;
+    spanbrojac.innerHTML = brojac;
+    uneseno.appendChild(paragraf);
+    input.value = "";
+    ukloni.addEventListener("click", function () {
+      uneseno.removeChild(paragraf);
+      spanbrojac.innerHTML = 0;
+    });
+  }
 });
